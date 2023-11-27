fd73f743afd8edc560b42c90493a529fa6eabf33
GLopes <gicaalopes2003@gmail.com>
2023-11-27 17:33:57 -0300

    rendering login page

--- ong-connect/src/main.jsx
+++ ong-connect/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Register from './pages/Register'
+import Login from './pages/Login'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <>
-      <Register title="Registro" />
+      <Login/>
     </>
   </React.StrictMode>,
 )
