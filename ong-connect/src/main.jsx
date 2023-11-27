e8a4a746f34a45fb9f660cd38ad34c1430f3b523
GLopes <gicaalopes2003@gmail.com>
2023-11-27 15:14:04 -0300

    rendering window component

--- ong-connect/src/main.jsx
+++ ong-connect/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Window from './components/window'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <>
+      <Window title="Faça sua consulta" />
+    </>
   </React.StrictMode>,
 )
