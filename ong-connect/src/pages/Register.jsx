553331b5b71a541fac53b8674736c338978ba1d0
GLopes <gicaalopes2003@gmail.com>
2023-11-27 21:26:16 -0300

    adjusting placement

--- ong-connect/src/pages/Register.jsx
+++ ong-connect/src/pages/Register.jsx
@@ -3,14 +3,23 @@ import logo from "../assets/logo.svg";
 
 const Register = (props) => {
   return (
-    <section className="p-4 window radius shadow">
-     <i className="bi bi-x float-end icon-window"></i>
+    <section className="position-absolute top-50 start-50 translate-middle p-4 window radius shadow">
+      <i className="bi bi-x float-end icon-window"></i>
       <header className="d-flex flex-column justify-content-center align-items-center">
         <img src={logo} />
         <p className="text-center fs-2 text-white mt-3">{props.title}</p>
       </header>
 
       <div className="container my-4">
+        <span className="d-flex justify-content-between">
+          <input className="bg-light w-25 me-2" type="text" placeholder="CPF" />
+          <input className="bg-light w-25" type="text" placeholder="RG" />
+          <input
+            className="bg-light w-50 ms-2"
+            type="text"
+            placeholder="Nacionalidade"
+          />
+        </span>
         <span className="d-flex justify-content-between">
           <input
             className="bg-light w-50 me-2"
@@ -22,21 +31,29 @@ const Register = (props) => {
             type="date"
             placeholder="Data de Nascimento"
           />
-          <input className="bg-light w-2 ms-2" type="text" placeholder="Sexo" />
+          <input
+            className="bg-light w-25 ms-2"
+            type="text"
+            placeholder="Sexo"
+          />
         </span>
         <span className="d-flex justify-content-between">
-          <input className="bg-light w-25" type="text" placeholder="CEP" />
-          <input className="bg-light w-25" type="text" placeholder="Cidade" />
-          <input className="bg-light w-25" type="text" placeholder="Estado" />
-        </span>
-        <span className="d-flex justify-content-between">
-          <input className="bg-light w-50 me-2" type="text" placeholder="Endereço" />
+          <input
+            className="bg-light w-50 me-2"
+            type="text"
+            placeholder="Endereço"
+          />
           <input
             className="bg-light w-25"
             type="text"
             placeholder="Complemento"
           />
           <input className="bg-light w-25 ms-2" type="text" placeholder="Nº" />
+        </span>
+        <span className="d-flex">
+          <input className="bg-light w-25" type="text" placeholder="CEP" />
+          <input className="bg-light w-25 ms-2" type="text" placeholder="Cidade" />
+          <input className="bg-light w-25 ms-2" type="text" placeholder="Estado" />
         </span>
         <span className="d-flex justify-content-center align-items-center">
           <button
